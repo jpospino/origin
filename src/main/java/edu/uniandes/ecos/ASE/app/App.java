b6eeaffa8eeb7e8ed483fdc39eff502b85e6f89a
Juan Pablo Ospino Solano <jp.ospino@uniandes.edu.co>
2017-02-15 20:48:14 -0500

    Segunda versión del proyecto adicionando e ignorando más archivos

---
+++ src/main/java/edu/uniandes/ecos/ASE/app/App.java
@@ -0,0 +1,16 @@
+package edu.uniandes.ecos.ASE.app;
+
+/**
+ * The Stack class represents a last-in-first-out stack of objects. 
+ * @author  Joseph Bergin
+ * @version 1.0, May 2000
+ * Note that this version is not thread safe. 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Prueba prueba = new Prueba();
+		prueba.printPantalla();
+    }
+}
---
+++ src/main/java/edu/uniandes/ecos/ASE/app/Prueba.java
@@ -0,0 +1,17 @@
+package edu.uniandes.ecos.ASE.app;
+
+/**
+ * The Stack class represents a last-in-first-out stack of objects. 
+ * @author  Joseph Bergin
+ * @version 1.0, May 2000
+ * Note that this version is not thread safe. 
+ */
+public class Prueba 
+{
+    public Prueba () {}
+	
+	public void printPantalla()
+	{
+		System.out.println( "Hello World!" );
+	}
+}
